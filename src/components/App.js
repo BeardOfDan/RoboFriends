3bc9e3af3b80866fad78f415441dcf162d37d2ab
Daniel Roberts <BeardOfDan@gmail.com>
2018-08-04 03:00:11 -0700

    (feat) Add a Searchbox component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import CardList from './CardList';
 import robots from './../robots.js';
+import Searchbox from './Searchbox';
 
 export default (props) => {
   return (
-    <CardList robots={robots} />
+    <div>
+      <h1>Robo Friends</h1>
+      <Searchbox />
+      <CardList robots={robots} />
+    </div>
   );
 };
