6969976497744eefd83d54a3a70a8c73b434b269
Daniel Roberts <BeardOfDan@gmail.com>
2018-08-04 03:49:59 -0700

    (refactor) Move font file to externalResources folder

--- src/components/App.js
+++ src/components/App.js
@@ -26,7 +26,7 @@ export default class App extends Component {
   render() {
     return (
       <div className='tc'>
-        <h1>Robo Friends</h1>
+        <h1 className='f1'>Robo Friends</h1>
         <Searchbox searchChange={this.onSearchChange} />
         <CardList robots={this.state.filteredRobots} />
       </div>
