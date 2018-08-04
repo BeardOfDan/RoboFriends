05514a4d70234028b0a05a3e52a24c9b0a37a436
Daniel Roberts <BeardOfDan@gmail.com>
2018-08-04 02:24:33 -0700

    (feat) Create Card component

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <img src='https://robohash.org/John_Doe?200x200' alt='robo friend' />
+      <div>
+        <h2>John Doe</h2>
+        <p>John.Doe@email.com</p>
+      </div>
+    </div>
+  );
+};
