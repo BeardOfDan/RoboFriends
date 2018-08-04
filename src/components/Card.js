3a303b09fdcc93a5bb1e8539cc55afba47491cdb
Daniel Roberts <BeardOfDan@gmail.com>
2018-08-04 02:26:49 -0700

    (feat) Add tachyons
    A 3rd party CSS stylings compilation

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => {
   return (
-    <div>
+    <div className='bg-light-green dib br3 pa3 ma2 grow'>
       <img src='https://robohash.org/John_Doe?200x200' alt='robo friend' />
       <div>
         <h2>John Doe</h2>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './components/Card';
 import registerServiceWorker from './registerServiceWorker';
+import 'tachyons'; // 3rd party CSS stylings
 
 ReactDOM.render(<Card />, document.getElementById('root'));
 registerServiceWorker();
