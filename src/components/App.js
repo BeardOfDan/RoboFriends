aaa80991d5cb83824039b56b5d321030157616fe
Daniel Roberts <BeardOfDan@gmail.com>
2018-08-06 20:33:15 -0700

    (refactor) Clean up render code

--- src/components/App.js
+++ src/components/App.js
@@ -36,20 +36,16 @@ export default class App extends Component {
   };
 
   render() {
-    if (this.state.robots.length === 0) {
-      return (
-        <h1 className="tc f1">Loading robots...</h1>
-      );
-    }
-
-    return (
-      <div className="tc">
-        <h1 className="f1">Robo Friends</h1>
-        <Searchbox searchChange={this.onSearchChange} />
-        <Scroll>
-          <CardList robots={this.state.filteredRobots} />
-        </Scroll>
-      </div>
-    );
+    return (this.state.robots.length) ?
+      (
+        <div className="tc">
+          <h1 className="f1">Robo Friends</h1>
+          <Searchbox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={this.state.filteredRobots} />
+          </Scroll>
+        </div>
+      )
+      : (<h1 className="tc f1">Loading robots...</h1>);
   }
 }
