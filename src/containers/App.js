c67dae96f3bba8d913473481e73eca7e2c634c2c
Daniel Roberts <BeardOfDan@gmail.com>
2018-08-10 21:36:59 -0700

    (refactor) Differentiate components from containers
    App.js is now in the containers folder
    The appropriate path changes have been made

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import Searchbox from './Searchbox';
-import Scroll from './Scroll';
-import ErrorBoundry from './ErrorBoundry';
+import CardList from '../components/CardList';
+import Searchbox from '../components/Searchbox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 import axios from 'axios';
 
