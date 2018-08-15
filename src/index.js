b5d072c3710a316a49f311b344b7a70b541b3fe0
Daniel Roberts <BeardOfDan@gmail.com>
2018-08-14 23:55:28 -0700

    (feat) Extend reducers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { Provider, connect } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
-import './index.css';
+import thunkMiddleware from 'redux-thunk';
+import { searchRobots } from './reducers';
+
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import { searchRobots } from './reducers';
+
+import './index.css';
 import 'tachyons';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
   <Provider store={store}>
