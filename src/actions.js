b09e3d9591abb82dadb4b0fa5e1bef0af0e79ae6
Daniel Roberts <BeardOfDan@gmail.com>
2018-08-14 22:46:39 -0700

    (fix) typo in src/actions.js

--- src/actions.js
+++ src/actions.js
@@ -2,7 +2,7 @@ import { CHANGE_SEARCH_FIELD } from './constants.js';
 
 export const setSearchField = (text) => {
   return {
-    'type': 'CHANGE_SEARCH_FIELD',
+    'type': CHANGE_SEARCH_FIELD,
     'payload': text
   };
 };
