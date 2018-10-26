3f1d7f4559ad0ddb64009d98abf970326a8838d2
Daniel Roberts <BeardOfDan@gmail.com>
2018-10-25 23:40:40 -0700

    (refactor) Replace shouldComponentUpdate with PureComponent
    Now, unnecessary renders are prevented by PureComponent, instead
    of by manual use of shouldComponentUpdate.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,6 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class Header extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return false; // this component is static text
-  }
-
+class Header extends PureComponent {
   render() {
     return <h1 className="f1">Robo Friends</h1>;
   }
