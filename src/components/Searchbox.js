7653d7bb7a6c58ce0e746c5c8971a42cff0be5f5
Daniel Roberts <BeardOfDan@gmail.com>
2018-10-26 16:44:18 -0700

    (feat) Give form an aria-label

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -7,6 +7,7 @@ export default ({ searchChange }) => {
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='Search Robots'
+        aria-label="Search Robots"
         onChange={searchChange}
       />
     </div>
