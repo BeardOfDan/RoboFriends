c2a87924303e1196a3e4a98a814e96331b26c17e
Daniel Roberts <BeardOfDan@gmail.com>
2018-10-27 20:34:31 -0700

    (feat) Don't perform logging in production

--- src/index.js
+++ src/index.js
@@ -13,11 +13,17 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import 'tachyons';
 
-const logger = createLogger();
-
 const rootReducer = combineReducers({ searchRobots, requestRobots });
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+// store is declared as a var to fix scope issue
+if (process.env.NODE_ENV !== 'production') { // use a logger
+  const logger = createLogger();
+  var store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+} else {
+  // eslint thinks store is being redefined
+  // eslint-disable-next-line
+  var store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+}
 
 ReactDOM.render(
   <Provider store={store}>
