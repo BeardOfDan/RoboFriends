6f2e57cd17ff07fe331995f274fc7711068c5132
Daniel Roberts <BeardOfDan@gmail.com>
2018-10-27 21:31:37 -0700

    (feat) Add tests for filterRobots

--- src/components/MainPage.test.js
+++ src/components/MainPage.test.js
@@ -18,4 +18,33 @@ describe('MainPage', () => {
   it('should render', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('filters robots correctly', () => {
+    // an empty robots array should have no results
+    expect(wrapper.instance().filterRobots()).toEqual([]);
+
+    // an array with a matched element should return a result
+    const mockProps = {
+      onRequestRobots: jest.fn(),
+      robots: [
+        {
+          id: 5,
+          name: 'John Snow',
+          email: 'kingOfTheNorth@gmail.com'
+        }
+      ],
+      searchField: 'o',
+      isPending: false
+    };
+
+    wrapper = shallow(<MainPage {...mockProps} />);
+
+    expect(wrapper.instance().filterRobots().length).toBe(1);
+
+    // a search that doesn't match any results should return no robots
+    mockProps.searchField = 'a';
+    wrapper = shallow(<MainPage {...mockProps} />);
+
+    expect(wrapper.instance().filterRobots().length).toBe(0);
+  });
 });
