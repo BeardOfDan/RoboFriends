9c335fe12b0f6a5d42a4b5d253649488cd5d853e
Daniel Roberts <BeardOfDan@gmail.com>
2018-10-28 22:08:31 -0700

    (test) REQUEST_ROBOTS_SUCCESS action

--- src/reducers.test.js
+++ src/reducers.test.js
@@ -36,4 +36,33 @@ describe('requestRobots', () => {
   it('should return the initial state', () => {
     expect(reducers.requestRobots(undefined, {})).toEqual(initialStateRobots);
   });
+
+  it('should handle REQUEST_ROBOTS_PENDING action', () => {
+    expect(reducers.requestRobots(initialStateRobots, {
+      type: REQUEST_ROBOTS_PENDING
+    })).toEqual({
+      robots: [],
+      isPending: true,
+      error: ''
+    });
+  });
+
+  it('should handle REQUEST_ROBOTS_SUCCESS action', () => {
+    expect(reducers.requestRobots(initialStateRobots, {
+      type: REQUEST_ROBOTS_SUCCESS,
+      payload: [{
+        id: '123',
+        name: 'test',
+        email: 'test@gmail.com'
+      }]
+    })).toEqual({
+      robots: [{
+        id: '123',
+        name: 'test',
+        email: 'test@gmail.com'
+      }],
+      isPending: false,
+      error: ''
+    });
+  });
 });
